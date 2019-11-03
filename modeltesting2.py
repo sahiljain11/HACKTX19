b8a3c8495057009f90d78f5d71bbd6cae19c7c0c
Daehyun Kyoung <danielkyoung2001@gmail.com>
2019-11-03 08:34:32 -0600

    modeltesting2

--- modeltesting2.py
+++ modeltesting2.py
@@ -4,7 +4,7 @@ Created on Sun Nov  3 03:43:00 2019
 
 @author: Danie
 """
-from PIL import Image
+from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 endpoint = 'https://image-classification.cognitiveservices.azure.com/'
 prediction_key = 'ba8e403c3f904d85adf408928b13fa90'
@@ -49,8 +49,30 @@ def find_damage(image):
             else:
                 row_result.append(False)
         result_matrix.append(row_result)
-    return result_matrix
+    new_image = combineimage(image_names,result_matrix,x,y)
+    return new_image
+
+def combineimage(section_matrix,damage_matrix,total_x,total_y):
+    x_size = int(total_x/5)
+    y_size = int(total_y/5)
+    new_image = Image.new('RGB',(total_x,total_y))
+    x = 0
+    y = 0
+    for row in range(0,5):
+        x = 0
+        new_row = Image.new('RGB',(total_x,y_size))
+        for col in range(0,5):
+            square = Image.open(section_matrix[row][col]).convert('RGB')
+            if damage_matrix[row][col] == 1:
+                draw = ImageDraw.Draw(square,'RGBA')
+                draw.rectangle(((0,0),(x_size,y_size)),fill = (255,0,0,100) , outline = None)
+            new_row.paste(square,(x,0))
+            x+=x_size
+        new_image.paste(new_row,(0,y))
+        y+=y_size
+    return new_image
+            
 
 with Image.open("C:\\Users\\Danie\\hacktx2019training2\\post_damage\\hurricane-florence_00000001_post_disaster.png") as image:
     result = find_damage(image)
-    print(result)
+    result.save('anewimage.png')
